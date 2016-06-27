372f75cab7200b7c1a19a0c65169e780ac53156d
jintan1123 <g1527040@st.t-kougei.ac.jp>
2016-06-27 18:21:06 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -14,6 +14,9 @@ for(var i = 1; i <= number; i++){
   var li = document.createElement('li');
   if(i%3===0){
     li.textContent ='Fizz'
+    if(i%5===0){
+    li.textContent ='Buzz'
+  }
   }else
   if(i%5===0){
     li.textContent ='Buzz'
