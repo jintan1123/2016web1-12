0901087b8faceac145e279136d9541bdb994f726
jintan1123 <g1527040@st.t-kougei.ac.jp>
2016-06-27 17:38:05 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,17 +1,20 @@
 var remainder3;
 var remainder5;
+var message;
 document.getElemenybyId('form').onsubmit = function(){
-  window.alert(document.getElementById('form').number.value);
 };
-for(var i = 1; i < 100; i++){
+for(var i = 1; i < number; i++){
   var li = document.createElement('li');
   remainder3 = i%3;
   remainder5 = i%5;
   if(remainder3=0){
+    message='Fizz'
   }
   if(remainder5=0){
+    message='Buzz'
   }
   else{
+    li.textContent = i;
   }
   document.getElementById('list').appendChild(li);
 }
